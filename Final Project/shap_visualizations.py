4270233efa055bcbaed3007753ce3ff6a11786ea
rshea33 <81315255+rshea33@users.noreply.github.com>
2022-12-17 15:30:18 -0500

    fixed a bug that would save figure before annotating

--- Final Project/shap_visualizations.py
+++ Final Project/shap_visualizations.py
@@ -26,16 +26,16 @@ def main():
 
     # Plot the SHAP values
     shap.summary_plot(shap_values, df, show=False)
-    plt.savefig('Visualizations/shap_summary.png')
     plt.title("Summary Plot for XGBoost Model")
     plt.annotate('The SHAP value represents the\n\
-        impact of a feature on the model output.\n\
-            The color represents the feature value\n\
-                (red high, blue low).', xy=(-3, 0.5), xytext=(0, 0), textcoords='offset points', ha='center', va='center', size=8)
+                 impact of a feature on the model output.\n\
+                 The color represents the feature value\n\
+                 (red high, blue low).', xy=(-3, 0.5), xytext=(0, 0), textcoords='offset points', ha='center', va='center', size=9)
     # Bold the top feature
-    plt.annotate('Most important Feature', xy=(2, 19.5), xytext=(0, 0), textcoords='offset points', ha='center', va='center', size=8, weight='bold')
-    plt.annotate('Least important Feature', xy=(2, -0.5), xytext=(0, 0), textcoords='offset points', ha='center', va='center', size=8, weight='bold')
+    plt.annotate('Most important Feature', xy=(2, 19.5), xytext=(0, 0), textcoords='offset points', ha='center', va='center', size=10, weight='bold', color='red')
+    plt.annotate('Least important Feature', xy=(2, -0.5), xytext=(0, 0), textcoords='offset points', ha='center', va='center', size=10, weight='bold', color='blue')
 
+    plt.savefig('Visualizations/shap_summary.png')
     print('Saved summary plot to Visualizations/shap_summary.png')
     plt.show()
 
